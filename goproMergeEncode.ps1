3cecec2775493605e11d534d5323ef6c345e70a8
alexandrosk0 <alexkonstantonis@gmail.com>
2019-04-21 23:26:53 -0400

    Updated documentation

--- goproMergeEncode.ps1
+++ goproMergeEncode.ps1
@@ -1,28 +1,30 @@
-#Gorpo file merger and encoding in Powershell using MKVToolNix and Handbrake
-#Merge and encode single videos split in multiple mp4 files in the given gopro export folder and removes original files if succesfull.
-#It can handle mulitple single videos in the same directory.
+#Merge and encode single videos split into multiple mp4 files in the given GoPro export folder and removes original files if successful.
+#It can handle multiple single videos in the same directory.
 #Needs MKVmerge https://mkvtoolnix.download/ and HandBrakeCLI https://handbrake.fr/downloads2.php
 #See official Handbrake presets https://handbrake.fr/docs/en/latest/technical/official-presets.html
 
 #Parameters
-#-dir Defaults to current directory
-#-preset Default preset "Vimeo YouTube HQ 1440p60 2.5K"
-#-MKVMergeExtraParam and -HandbrakeExtraParam Extra parameters for each program 
+#-dir Defaults to the current directory
+#-preset The default preset is "Vimeo YouTube HQ 1440p60 2.5K"
+#-MKVMergeExtraParam Extra parameters for MKVMerge
+#-HandbrakeExtraParam Extra parameters for Handbrake
 #-test Test merge and encode three seconds of each video, without deleting the original files
 #-delete Delete original files if not error is returned. Defaults to true
 #-mergeAll Ignore file naming and merge all files in the directory
 #-noEncoding Skip encoding step
 
 #Based on the file name format GOPR2548.MP4 and multiple files GP012548.MP4, GP022548.MP4,... and folder structure ..\GoPro\2019-04-21\HERO5 Black 1\
-#Note that the stucture works for single videos for the camera models: HD HERO2, HERO3, HERO3+, HERO (2014), HERO Session, HERO4, HERO5 Black, HERO5 Session, HERO (2018)
+#Note that the script expects a structure that works for single videos for the camera models: HD HERO2, HERO3, HERO3+, HERO (2014), HERO Session, HERO4, HERO5 Black, HERO5 Session, HERO (2018)
 #For HERO6 Black, & HERO7 (White, Silver, Black) or other formats use -mergeAll parameter
 #See GoPro Camera File Naming Convention https://gopro.com/help/articles/question_answer/GoPro-Camera-File-Naming-Convention
 
-#Example .\goproMergeEncode1440p60.ps1 -dir "D:\Video\GoPro\2019-04-21\HERO5 Black 1\" -preset "Vimeo YouTube HQ 1440p60 2.5K"
+#Example .\goproMergeEncode.ps1 -dir "D:\Video\GoPro\2019-04-21\HERO5 Black 1\"
 #Output file for D:\Video\GoPro\2019-04-21\HERO5 Black 1\GOPR2548.MP4 is Encoded-GOPR2548-20190421.mp4
+#If you have issues running powershell scripts, use powershell –ExecutionPolicy Bypass or see https://www.windowscentral.com/how-create-and-run-your-first-powershell-script-file-windows-10
+# Author: Alexandros Konstantonis alexkonstantonis(at)gmail.com
 
 param (
-    [string]$dir = $(Get-Location),
+	[string]$dir = $(Get-Location),
 	[string]$preset = 'Vimeo YouTube HQ 1440p60 2.5K',
 	[string]$MKVMergeExtraParam = '',
 	[string]$HandbrakeExtraParam = '',
